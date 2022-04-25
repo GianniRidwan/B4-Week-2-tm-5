376bc881710751772d27b2a192029d43577be140
Gianni Ridwan <99036552@mydavinci.nl>
2022-04-25 11:09:18 +0200

    lab afgemaakt

--- Lab medewerkers MVC/medewerkers_in_mvc/view/employee/delete.php
+++ Lab medewerkers MVC/medewerkers_in_mvc/view/employee/delete.php
@@ -6,7 +6,6 @@ if (isset($employee)) $employee = $employee;
 
 <h1>Medewerker verwijderen</h1>
     <p>Weet je zeker dat je <?= $employee['name'] ?> wil verwijderen?</p>
-    <p>ID: #<?= $employee['id'] ?></p>
         <div class="row">
             <a href="<?= URL ?>employee/index" class="btn btn-primary">Annuleer</a>
             <a href="<?= URL ?>employee/destroy/<?= $employee['id'] ?>"class="btn btn-danger">Verwijder</a>
